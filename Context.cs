73c513e3b85ba0c6ef7a5b4d28f749a31d4a1c17
royms <hadash04@gmail.com>
2019-07-16 14:33:32 +0300

    momento

---
+++ Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void Load(RichTextBox richText)
+        {
+            if (careTake.Memento != null)
+            {
+                var last = careTake.GetLast();
+                if (last.savedText != null)
+                    richText.Text = last.savedText;
+            }
+        }
+
+        public void Redo(RichTextBox richText)
+        {
+            if (careTake.Memento != null)
+            {
+                var next = careTake.GetNext();
+                if (next.savedText != null)
+                    richText.Text = next.savedText;
+            }
+        }
+    }
+}
---
+++ TextCareTaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        public List<TextMemento> Memento = new List<TextMemento>();
+        public int index = 0;
+
+        public void AddMemento(TextMemento memento)
+        {
+            if (index < this.Memento.Count)
+            {
+                this.Memento.RemoveRange(index, this.Memento.Count - index);
+            }
+
+            this.Memento.Add(memento);
+            index++;
+        }
+
+        public TextMemento GetLast()
+        {
+            if (index > 1)
+                index--;
+
+            return this.Memento[index-1];
+        }
+
+        public TextMemento GetNext()
+        {
+            if (index < Memento.Count)
+                index++;
+
+            return this.Memento[index-1];
+        }
+    }
+}
